583708b4e6b6f379dc7dd3c02acd677259f9bfb8
amjad <na80@hotmail.com>
2022-05-30 23:04:03 +0200

    update

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/user/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist 
+
+git init
+git add -A
+git commit -m "deploy"
+git push -f git@github.com:amjadnatouf/blogger.git master:gh-pages
+
+cd -
